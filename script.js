f0f2a7690e1da5f987e1667ebf69be350063480c
millivanzo <112878568+millivanzo@users.noreply.github.com>
2024-07-25 16:48:44 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
+
+const perguntas[
+    {
+        enuciado:"Assinale a alternativa que apresenta a melhor conceituação do termo meio ambiente:"
+        alternativas:[
+            ¨É a inter-relação entre os diversos componentes físicos e humanos.¨
+            ¨É a relação entre os aspectos de origem antrópica do espaço social.¨
+        ]
+
+    }
+]
